c6618538c033a02d5e087ea794eebe59d81b0b97
jayesh-patil7066 <jayesh9507@gmail.com>
2024-02-21 16:53:23 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState, useCallback } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [length, setlenghth] = useState(8);
+  const [numAllow, setnumAllow] = useState(false);
+  const [charAllow, setcharAllow] = useState(false);
+  const [password, setPassword] = useState("");
 
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numAllow) str += "0123456789";
+    if (charAllow) str += "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+    for (let i = 0; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numAllow, charAllow, setPassword]);
   return (
     <>
-      practic
-      
-       
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700">
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+          />
+        </div>
+      </div>
     </>
   );
 }
